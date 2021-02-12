22ac513cbfcadce8a8f950e8674f8107afee002c
Swapna <sama.swapna@edu.sait.ca>
2021-02-12 10:15:03 -0700

    ES6 syntax change and changed text in button

--- js/global.js
+++ js/global.js
@@ -9,23 +9,23 @@ form.addEventListener('submit', function(event){
   myResult.style.color="blue";
   if(mathSelect=== 1){
     result = num1 + num2;
-    myResult.innerHTML="Sum is " + result ;
+    myResult.innerHTML=`Sum is ${result}`;
   } 
   else if(mathSelect===2){
     result = num1 - num2;
-    myResult.innerHTML="Difference is " + result ;
+    myResult.innerHTML=`Difference is ${result}` ;
   }
   else if(mathSelect===3){
     result = num1 * num2;
-    myResult.innerHTML="Product is " + result ;
+    myResult.innerHTML=`Product is ${result}` ;
   }
   else if(mathSelect===4){
-    result = num1 / num2;;
-    myResult.innerHTML="Quotient is " + result ;
+    result = num1 / num2;
+    myResult.innerHTML=`Quotient is ${result}` ;
   }
   else{
     myResult.style.color="red";
-    myResult.innerHTML="Please select a valid math option" ;
+    myResult.innerHTML=`Please select a valid math option` ;
   }
 
   event.preventDefault();
